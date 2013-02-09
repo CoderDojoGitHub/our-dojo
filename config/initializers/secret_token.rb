13c41f2af34bf438ccb859f0536d2fbda310a8b8
Jonathan Hoyt <hoyt@github.com>
2013-02-09 13:54:10 -0800

    Gimme dat Rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CoderdojoWebapp::Application.config.secret_token = 'd67e0d88203bba7b759d15b2f43697cf8bd494970295347f6a8ab0d9b3a6b8919f35d7e0b1102b1314fe194dde76cf1963dea5dfeb3f7a22f2e08da0b817a6b5'
