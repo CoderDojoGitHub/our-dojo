ca51f9e4308999a6bbe5d92970065cfc2f6dd11e
Jonathan Hoyt <hoyt@github.com>
2013-02-09 19:01:00 -0800

    Add authentication for administration.

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ ruby "1.9.3"
 gem "rails"
 gem "jquery-rails"
 gem "dotenv"
+gem "omniauth-github-team-member"
 
 group :assets do
   gem "coffee-rails"
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,38 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :authenticate_user!
+
+  def home
+    render text: "Hello World"
+  end
+
+  def current_user
+    @current_user ||= User.find_by_id(session[:user_id])
+  end
+  helper_method :current_user
+
+  def signed_in?
+    !!current_user
+  end
+
+  def sign_in_and_redirect(user)
+    self.current_user = user
+    redirect_to session[:user_return_to] || root_path
+  end
+
+  def current_user=(user)
+    @current_user = user
+    session[:user_id] = user.id
+  end
+
+  # Require that the user is signed in to view
+  # the current action.
+  #
+  # Redirects to login if the user is not signed in.
+  def authenticate_user!
+    if !signed_in?
+      session[:user_return_to] = request.path if request.path
+      redirect_to "/auth/githubteammember"
+    end
+  end
 end
