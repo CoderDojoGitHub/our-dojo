7bd730f29fc04547c3ae32c5f8f36dc92571295f
Jonathan Hoyt <hoyt@github.com>
2013-04-21 14:21:14 -0700

    Change to ruby 1.9 syntax.

--- db/migrate/20130210032508_create_events.rb
+++ db/migrate/20130210032508_create_events.rb
@@ -1,13 +1,13 @@
 class CreateEvents < ActiveRecord::Migration
   def change
     create_table :events do |t|
-      t.string :title
-      t.datetime :start_time
-      t.datetime :end_time
-      t.integer :state, default: 0
-      t.datetime :scheduled_at
-      t.datetime :invites_sent_at
-      t.datetime :opened_registration_at
+      t.string    :title
+      t.datetime  :start_time
+      t.datetime  :end_time
+      t.integer   :state, default: 0
+      t.datetime  :scheduled_at
+      t.datetime  :invites_sent_at
+      t.datetime  :opened_registration_at
 
       t.timestamps
     end
