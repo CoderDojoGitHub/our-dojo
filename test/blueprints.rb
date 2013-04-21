a464df70b0f87cb5842369b6b8e7e43e3dd52ecc
Jonathan Hoyt <hoyt@github.com>
2013-04-21 14:31:54 -0700

    Add Lesson blueprint.

--- test/blueprints.rb
+++ test/blueprints.rb
@@ -1,6 +1,12 @@
 require "machinist/active_record"
 require "faker"
 
+Lesson.blueprint do
+  title { "Lesson #{sn}" }
+  summary { "Learn crazy awesome #{sn} when you use #{sn}."}
+  repository { "lesson-#{sn}" }
+end
+
 Event.blueprint do
   title { "Event #{sn}" }
   start_time { 3.weeks.from_now }
