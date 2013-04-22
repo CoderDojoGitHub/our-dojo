e394e502c70b44d73943f403e83a7e98ffdd7682
Jonathan Hoyt <hoyt@github.com>
2013-04-22 14:54:58 -0700

    Add events to Lesson blueprint.

--- test/blueprints.rb
+++ test/blueprints.rb
@@ -5,6 +5,16 @@ Lesson.blueprint do
   title { "Lesson #{sn}" }
   summary { "Learn crazy awesome #{sn} when you use #{sn}."}
   repository { "lesson-#{sn}" }
+  events {
+    [
+      {
+        "location" => "GitHub 548 4th St San Francisco CA 94107",
+        "date" => 1.week.from_now.to_s,
+        "length_in_hours" => 2,
+        "teacher_github_username" => "jonmagic"
+      }
+    ]
+  }
 end
 
 Event.blueprint do
