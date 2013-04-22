f54239e8050350111aa9ebf2e81916ec4cb7eabb
Jonathan Hoyt <hoyt@github.com>
2013-04-22 13:26:37 -0700

    Add lesson importer.

---
+++ app/models/lesson_importer.rb
@@ -0,0 +1,27 @@
+class LessonImporter
+  attr_reader :organization
+
+  # Public: Called by .new while instantiating the object.
+  #
+  # organization = Organization object to import from.
+  def initialize(organization)
+    @organization = organization
+  end
+
+  # Public: Import lessons from lesson repositories in the organization.
+  #
+  # Returns an Array.
+  def import
+    lesson_repositories.select do |lesson_repository|
+      lesson_attributes = lesson_repository.lesson
+      lesson = Lesson.find_or_initialize_by_repository(lesson_repository.name)
+      lesson.title = lesson_attributes["title"]
+      lesson.summary = lesson_attributes["summary"]
+      lesson.save
+    end
+  end
+
+  def lesson_repositories
+    @lesson_repositories ||= organization.lesson_repositories
+  end
+end
---
+++ test/models/lesson_importer_test.rb
@@ -0,0 +1,33 @@
+require "minitest_helper"
+
+describe LessonImporter do
+  describe "#import" do
+    it "creates lessons for lesson repositories" do
+      VCR.use_cassette "coderdojosf/Particles/lesson.json import" do
+        organization = Organization.new("coderdojosf")
+        importer = LessonImporter.new(organization)
+
+        assert_equal 0, Lesson.count
+        importer.import
+        assert_equal 1, Lesson.count
+
+        lesson = Lesson.first
+        assert_equal "Animations in Javascript", lesson.title
+        assert_equal "Build animations in the web browser using javascript and the D3.js library.", lesson.summary
+        assert_equal "Particles", lesson.repository
+      end
+    end
+
+    it "does not duplicate lesson repositories" do
+      VCR.use_cassette "coderdojosf/Particles/lesson.json import twice" do
+        organization = Organization.new("coderdojosf")
+        importer = LessonImporter.new(organization)
+
+        importer.import
+        assert_equal 1, Lesson.count
+        importer.import
+        assert_equal 1, Lesson.count
+      end
+    end
+  end
+end
