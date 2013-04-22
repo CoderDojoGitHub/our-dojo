440a9ae6a78d4205711ba395a73da2be8daa9694
Jonathan Hoyt <hoyt@github.com>
2013-04-22 15:39:10 -0700

    Refactor EventImporter.

--- app/models/event_importer.rb
+++ app/models/event_importer.rb
@@ -8,27 +8,44 @@ class EventImporter
     @lessons = lessons
   end
 
+  # Public: Import events from lessons.
+  #
+  # Returns an Array.
   def import
     lessons.map do |lesson|
-      next unless lesson.events.present?
-
-      lesson.events.map do |event_attributes|
-        start_time = Time.parse(event_attributes["date"])
-        event_date = start_time.strftime("%Y%m%d")
-        event_slug = "#{lesson.repository}-#{event_date}"
-        event = Event.find_or_initialize_by_slug(event_slug)
-        event.start_time = start_time
-        event.teacher_github_username = event_attributes["teacher_github_username"]
-
-        if event["length_in_hours"].present?
-          event.end_time = start_time + event_attributes["length_in_hours"].hours
-        end
-
-        if event.changed?
-          event.save
-          event
-        end
-      end
+      process_events(lesson, lesson.events)
     end.flatten.compact
+  end
+
+  # Internal: Process events.
+  #
+  # Returns an Array.
+  def process_events(lesson, events)
+    return if events.blank?
+
+    lesson.events.map do |event_attributes|
+      process_event(event_attributes, lesson.repository)
+    end
+  end
+
+  # Internal: Find and update or create event.
+  #
+  # Returns an Event or NilClass.
+  def process_event(event_attributes, repository)
+    start_time = Time.parse(event_attributes["date"])
+    event_date = start_time.strftime("%Y%m%d")
+    event_slug = "#{repository}-#{event_date}"
+    event = Event.find_or_initialize_by_slug(event_slug)
+    event.start_time = start_time
+    event.teacher_github_username = event_attributes["teacher_github_username"]
+
+    if event["length_in_hours"].present?
+      event.end_time = start_time + event_attributes["length_in_hours"].hours
+    end
+
+    if event.changed?
+      event.save
+      event
+    end
   end
 end
