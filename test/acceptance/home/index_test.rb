c904374876fc4fed9ce67075b815e20a0b24537c
Jonathan Hoyt <hoyt@github.com>
2013-04-23 08:00:00 -0700

    Fix failing view test.

--- test/acceptance/home/index_test.rb
+++ test/acceptance/home/index_test.rb
@@ -4,7 +4,6 @@ class HomeIndexTest < AcceptanceTest
   it "renders the homepage" do
     visit "/"
     must_have_content "CoderDojo - San Francisco"
-    must_have_content "Upcoming Event"
     must_have_content "Upcoming Events Newsletter"
   end
 end
