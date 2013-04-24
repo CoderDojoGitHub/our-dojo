c974cf186f18992bb06f605866b9fcc949d4e786
Jonathan Hoyt <hoyt@github.com>
2013-04-24 09:00:24 -0700

    Configure actionmailer for dev & prod environments.
    Configured to use smtp and Mandrill by Mailchimp.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,14 @@ CoderdojoWebapp::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      :port =>           '587',
+      :address =>        'smtp.mandrillapp.com',
+      :user_name =>      ENV['MANDRILL_USERNAME'],
+      :password =>       ENV['MANDRILL_APIKEY'],
+      :domain =>         'coderdojosf.herokuapp.com',
+      :authentication => :plain
+  }
 end
