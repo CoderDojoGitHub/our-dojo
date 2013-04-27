bfcac94b8f60572b0f84c7d57facd8747fd8c33f
Jonathan Hoyt <hoyt@github.com>
2013-04-27 12:36:17 -0700

    Set ActionMailer default_url_options for all environments.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,6 +35,7 @@ CoderdojoWebapp::Application.configure do
   # Expands the lines which load the assets
   config.assets.debug = true
 
+  config.action_mailer.default_url_options = {host: "localhost", port: "3000"}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
       :port =>           '587',
