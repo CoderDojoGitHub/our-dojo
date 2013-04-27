df6016eb232d38d9a07c570097b075809366e1ad
Jonathan Hoyt <hoyt@github.com>
2013-04-27 16:33:57 -0700

    Remove Newsletter form for now.

--- test/acceptance/home/index_test.rb
+++ test/acceptance/home/index_test.rb
@@ -4,6 +4,6 @@ class HomeIndexTest < AcceptanceTest
   it "renders the homepage" do
     visit "/"
     must_have_content "CoderDojo - San Francisco"
-    must_have_content "Upcoming Events Newsletter"
+    must_have_content "View all lessons"
   end
 end
