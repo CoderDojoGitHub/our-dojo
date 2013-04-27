0814820f6067a70a18afa7a7554d1624fe96635a
Jonathan Hoyt <hoyt@github.com>
2013-04-27 16:52:27 -0700

    Include User-Agent in requests.
    Seems this is required now.

--- app/models/git_hub_api.rb
+++ app/models/git_hub_api.rb
@@ -5,7 +5,11 @@ class GitHubApi
   #
   # Returns an Array or Hash.
   def self.api_call(path)
-    response = HTTParty.get("#{base_url}/#{path}")
+    response = HTTParty.get("#{base_url}/#{path}", {
+      headers: {
+        "User-Agent" => user_agent
+      }
+    })
     ActiveSupport::JSON.decode(response.body)
   end
 
@@ -15,7 +19,11 @@ class GitHubApi
   #
   # Returns an Integer.
   def self.code(path)
-    response = HTTParty.get("#{base_url}/#{path}")
+    response = HTTParty.get("#{base_url}/#{path}", {
+      headers: {
+        "User-Agent" => user_agent
+      }
+    })
     response.code
   end
 
@@ -26,15 +34,19 @@ class GitHubApi
   # Returns an Array or Hash.
   def self.parsed_file_contents(path)
     response = HTTParty.get("#{base_url}/#{path}", {
-      query: {},
       headers: {
-        "accept" => "application/vnd.github-blob.raw"
+        "accept" => "application/vnd.github-blob.raw",
+        "User-Agent" => user_agent
       }
     })
     # ActiveSupport::JSON.decode(response.body)
     # FIX: Don't use YAML.load here. Only used because
     # ActiveSupport::JSON was choking.
     YAML.load(response.body)
+  end
+
+  def self.user_agent
+    ENV["GITHUB_ORGANIZATION"]
   end
 
   # Internal: Base url to GitHub API.
