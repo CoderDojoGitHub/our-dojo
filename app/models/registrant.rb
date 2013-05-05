740dbb7fb1b3d65e2bf335eb42ea7d78c5e2966c
Jonathan Hoyt <hoyt@github.com>
2013-05-04 18:20:08 -0700

    Remove attr_accessible in Registrant.

--- app/models/registrant.rb
+++ app/models/registrant.rb
@@ -3,7 +3,6 @@ require "email_validator"
 class Registrant < ActiveRecord::Base
   # Public: Email of the registrant. Must be present and a valid email.
   # column :email
-  attr_accessible :email
   validates :email, presence: true, email: true
 
   # Public: Has many temporary registrations.
