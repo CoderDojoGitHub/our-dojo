2db227bd9c9454fa31da78ee97126ebaab58200b
Jonathan Hoyt <hoyt@github.com>
2013-05-05 14:21:31 -0700

    Start using Mailgun.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,11 +38,11 @@ CoderdojoWebapp::Application.configure do
   config.action_mailer.default_url_options = {host: "localhost", port: "3000"}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-      :port =>           '587',
-      :address =>        'smtp.mandrillapp.com',
-      :user_name =>      ENV['MANDRILL_USERNAME'],
-      :password =>       ENV['MANDRILL_APIKEY'],
-      :domain =>         'coderdojosf.herokuapp.com',
-      :authentication => :plain
+    address:         ENV["MAILGUN_SMTP_SERVER"],
+    port:            ENV["MAILGUN_SMTP_PORT"],
+    domain:          ENV["MAILGUN_DOMAIN"],
+    user_name:       ENV["MAILGUN_SMTP_LOGIN"],
+    password:        ENV["MAILGUN_SMTP_PASSWORD"],
+    authentication:  "plain"
   }
 end
