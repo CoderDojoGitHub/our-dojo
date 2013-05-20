399c82e13771a5dfd0fe6b2714bcc1579a3b186d
Jonathan Hoyt <hoyt@github.com>
2013-05-20 11:56:27 -0700

    Document authentication methods.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,20 +1,33 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  # Public: The signed in user. This is available in views and helpers.
+  #
+  # Returns a User.
   def current_user
     @current_user ||= User.find_by_id(session[:user_id])
   end
   helper_method :current_user
 
+  # Public: Is the visitor signed in?
+  #
+  # Returns a TrueClass or FalseClass.
   def signed_in?
     !!current_user
   end
 
+  # Public: Set current_user equal to user and redirect to url in session
+  # or to the root path.
+  #
+  # user - User instance.
   def sign_in_and_redirect(user)
     self.current_user = user
     redirect_to session[:user_return_to] || root_path
   end
 
+  # Internal: Sets @current_user to user and user_id in session.
+  #
+  # user - User instance.
   def current_user=(user)
     @current_user = user
     session[:user_id] = user.id
