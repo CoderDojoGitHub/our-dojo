1c5838f1866065aac9289c40fb252108d6c84c87
Jonathan Hoyt <hoyt@github.com>
2014-01-04 10:46:19 -0800

    Add lesson builder to active admin.

--- app/admin/lessons.rb
+++ app/admin/lessons.rb
@@ -9,3 +9,13 @@ ActiveAdmin.register Lesson do
     actions
   end
 end
+
+ActiveAdmin.register_page "Lesson Generator" do
+  content do
+    render partial: "admin/lesson_builder"
+  end
+
+  sidebar :result do
+    render partial: "admin/result"
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -24,9 +24,6 @@ CoderdojoWebapp::Application.routes.draw do
     mount MailPreview => "mail_view"
   end
 
-  # Admin routes.
-  get "/admin", to: "admin/lesson_builder#index"
-
   # Default routes.
   root to: "home#index"
 end
