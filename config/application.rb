bd8861327a3bf3e15ea7e1e6a2a1a9580f9b078b
Jonathan Hoyt <hoyt@github.com>
2014-01-04 19:46:44 -0800

    Only print message in test and development env.

--- config/application.rb
+++ config/application.rb
@@ -29,7 +29,7 @@ module CoderdojoWebapp
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     if ENV["TZ"]
       config.time_zone = ENV["TZ"]
-    else
+    elsif Rails.env.test? || Rails.env.development?
       puts "\n=====================\n\n"
       puts "Whoops! You need to uncomment TZ and set it to the appropriate timezone of your Dojo."
       puts "For more information about possible timezones, run: rake time:zones:all."
